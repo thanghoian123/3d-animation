0a2513defd44e309e8805118be5050e5b9f838f7
thanghoian123 <57632755+thanghoian123@users.noreply.github.com>
2020-10-29 10:15:40 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const card = document.querySelector('.card');
+const container = document.querySelector('.container');
+const title = document.querySelector('.title');
+const sneaker = document.querySelector('.sneaker img');
+const circle = document.querySelector('.circle');
+const purchase = document.querySelector('.purchase button');
+const description = document.querySelector('.info h3');
+const sizes = document.querySelector('.sizes');
+
+container.addEventListener('mousemove', (e)=>{
+    let xAxis = (window.innerWidth/2 -e.pageX)/10;
+    let yAxis = (window.innerHeight/2 -e.pageY)/10;
+    card.style.transform = `rotateY(${yAxis}deg) rotateX(${xAxis}deg)`;
+    
+})
+
+container.addEventListener('mouseenter', (e)=>{
+    card.style.transition = "none";
+    title.style.transform='translateZ(70px)';
+    sneaker.style.transform='translateZ(100px)  rotate(-45deg)';
+    description.style.transform='translateZ(50px)';
+})
+
+container.addEventListener('mouseleave', (e)=>{
+    card.style.transition = "all 0.5s linear";
+    card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+
+    title.style.transform='translateZ(0px)';
+    sneaker.style.transform='translateZ(0px)  rotate(0deg)';
+    description.style.transform='translateZ(0px)';
+})
